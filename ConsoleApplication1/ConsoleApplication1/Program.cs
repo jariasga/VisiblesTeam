2f569ad4c785f796d08b0439026d7021c86f796f
Javier Arias <skeletpiece@hotmail.com>
2017-04-23 16:06:03 -0500

    test

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        //public Worker first_worker { get; protected set; }
+        static void Main(string[] args)
+        {
+            Worker first_worker = new Worker("camila", "vente");
+            Console.WriteLine(first_worker.getFullName()); 
+    
+            using (var fs = File.OpenRead("prueba ratios.csv"))
+            using (var reader = new StreamReader(fs))
+            {
+                List<string> tipos = new List<string>();
+                List<string> procesos = new List<string>();
+                List<string> productos = new List<string>();
+                List<string> valores = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    Console.WriteLine(values[0]);
+
+                    tipos.Add(values[0]);
+                    procesos.Add(values[1]);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Worker
+    {
+        public string name;
+        public string lastname;
+        public List<Ratio> ratios;
+
+        public Worker(string name, string lastname)
+        {
+            ratios = new List<Ratio>();
+            this.name = name;
+            this.lastname = lastname;
+        }
+
+        public string getFullName()
+        {
+            return name + " " + lastname;
+        }
+        
+    }
+}
