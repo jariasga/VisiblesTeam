ebf304f0d75cec9de87c75b49d8faaadd2ffe619
camilamilagros <camila.chavez@pucp.pe>
2017-04-24 01:59:02 -0500

    sources and general clases

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,26 +13,12 @@ namespace ConsoleApplication1
         //public Worker first_worker { get; protected set; }
         static void Main(string[] args)
         {
-            Worker first_worker = new Worker("camila", "vente");
-            Console.WriteLine(first_worker.getFullName()); 
-    
-            using (var fs = File.OpenRead("prueba ratios.csv"))
-            using (var reader = new StreamReader(fs))
-            {
-                List<string> tipos = new List<string>();
-                List<string> procesos = new List<string>();
-                List<string> productos = new List<string>();
-                List<string> valores = new List<string>();
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    var values = line.Split(';');
-                    Console.WriteLine(values[0]);
+            int painting = 10;
+            int baking = 10;
+            int carving = 10;
 
-                    tipos.Add(values[0]);
-                    procesos.Add(values[1]);
-                }
-            }
+            Instance instance = new Instance(painting, baking, carving);
+            instance.print();
             Console.Read();
         }
     }
