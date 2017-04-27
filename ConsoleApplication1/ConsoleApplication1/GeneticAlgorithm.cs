9e73f1b696793985ef26b72c5ea354ab1b77f937
Reechee <ricardo751991@gmail.com>
2017-04-27 00:08:24 -0500

    inicio branch

--- ConsoleApplication1/ConsoleApplication1/GeneticAlgorithm.cs
+++ ConsoleApplication1/ConsoleApplication1/GeneticAlgorithm.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace ConsoleApplication1
 {
+    
     class GeneticAlgorithm
     {
+        public List<List<int>> FirstGen;
+
+        public GeneticAlgorithm() {
+            FirstGen = new List<List<int>>();
+        }
+        public void CreateFirstGen() //Cargar la primera generacion
+        { 
+
+            using (var fs = File.OpenRead("vi.csv"))
+            using (var reader = new StreamReader(fs))
+            {
+                while (!reader.EndOfStream)
+                {
+                    List<int> member = new List<int>(); //Individuo de la generacion
+                    var line = reader.ReadLine();
+                    Console.WriteLine(line);
+                    var values = line.Split(',');
+                    for (int i = 0; i < values.Count(); i++) {
+                        member.Add(int.Parse(values[i]));
+                    }
+                 FirstGen.Add(member);
+                }
+            }
+        }
+
+        public void RunGenetic() //Inicio del programa Genetico
+        {
+
+
+
+        }
+
+
+        public void Print() //imprime los resultados
+        {
+            for(int i = 0; i < FirstGen.Count(); i++)
+            {
+                for(int j= 0; j < FirstGen[i].Count(); j++)
+                {
+                    Console.Write(FirstGen[i][j] + ", ");
+                }
+                Console.WriteLine(i);
+            }
+
+        }
+
+
+
     }
 }
