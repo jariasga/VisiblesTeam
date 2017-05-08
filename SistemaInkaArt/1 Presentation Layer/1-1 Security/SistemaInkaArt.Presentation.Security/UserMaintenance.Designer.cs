3cf7aaf0130f74c2789949df466eb2e025cfbedd
Javier Arias <skeletpiece@hotmail.com>
2017-05-08 05:38:17 -0500

    +

--- SistemaInkaArt/1 Presentation Layer/1-1 Security/SistemaInkaArt.Presentation.Security/UserMaintenance.Designer.cs
+++ SistemaInkaArt/1 Presentation Layer/1-1 Security/SistemaInkaArt.Presentation.Security/UserMaintenance.Designer.cs
@@ -66,6 +66,10 @@
             this.tableAdapterManager = new Presentation.Security.PersonDataSetTableAdapters.TableAdapterManager();
             this.userTableAdapter = new Presentation.Security.PersonDataSetTableAdapters.UserTableAdapter();
             this.turnTableAdapter = new Presentation.Security.PersonDataSetTableAdapters.TurnTableAdapter();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tabControl1.SuspendLayout();
@@ -279,6 +283,8 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button4);
             this.tabPage2.Controls.Add(this.comboBoxUserStatus);
             this.tabPage2.Controls.Add(this.label6);
             this.tabPage2.Controls.Add(this.textBoxUsername);
@@ -410,6 +416,8 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.listBoxUserList);
             this.groupBox2.Location = new System.Drawing.Point(557, 18);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -429,7 +437,7 @@
             this.listBoxUserList.Location = new System.Drawing.Point(8, 31);
             this.listBoxUserList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBoxUserList.Name = "listBoxUserList";
-            this.listBoxUserList.Size = new System.Drawing.Size(199, 184);
+            this.listBoxUserList.Size = new System.Drawing.Size(199, 264);
             this.listBoxUserList.TabIndex = 0;
             this.listBoxUserList.ValueMember = "idPerson";
             // 
@@ -452,6 +460,58 @@
             // turnTableAdapter
             // 
             this.turnTableAdapter.ClearBeforeFill = true;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Firebrick;
+            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(220, 80);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 39);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Desactivar";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gray;
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(119, 305);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(8, 305);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.SteelBlue;
+            this.button4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(220, 24);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 39);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Nuevo";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // UserMaintenance
             // 
@@ -522,5 +582,9 @@
         private System.Windows.Forms.ComboBox comboBoxUserTurn;
         private System.Windows.Forms.BindingSource turnBindingSource;
         private PersonDataSetTableAdapters.TurnTableAdapter turnTableAdapter;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
