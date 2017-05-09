79c4c8fc29cad78aee724b8956fe339c90df60f8
camilamilagros <camila.chavez@pucp.pe>
2017-05-09 12:27:57 -0500

    Revert "Camila"

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,12 +13,26 @@ namespace ConsoleApplication1
         //public Worker first_worker { get; protected set; }
         static void Main(string[] args)
         {
-            int painting = 10;
-            int baking = 10;
-            int carving = 10;
+            Worker first_worker = new Worker("camila", "vente");
+            Console.WriteLine(first_worker.getFullName()); 
+    
+            using (var fs = File.OpenRead("prueba ratios.csv"))
+            using (var reader = new StreamReader(fs))
+            {
+                List<string> tipos = new List<string>();
+                List<string> procesos = new List<string>();
+                List<string> productos = new List<string>();
+                List<string> valores = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    Console.WriteLine(values[0]);
 
-            Instance instance = new Instance(painting, baking, carving);
-            instance.print();
+                    tipos.Add(values[0]);
+                    procesos.Add(values[1]);
+                }
+            }
             Console.Read();
         }
     }
