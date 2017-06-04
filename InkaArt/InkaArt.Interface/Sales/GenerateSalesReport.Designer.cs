a1dcdbf1ae0500ba577027fd80ae52420c649ec2
flordemaria <fnoriegac@pucp.pe>
2017-06-04 18:20:04 -0500

    sales report

--- InkaArt/InkaArt.Interface/Sales/GenerateSalesReport.Designer.cs
+++ InkaArt/InkaArt.Interface/Sales/GenerateSalesReport.Designer.cs
@@ -33,27 +33,27 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button_generate = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.dateTimePicker2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.dateTimePicker1);
             this.groupBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(28, 44);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(755, 359);
+            this.groupBox1.Size = new System.Drawing.Size(389, 298);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Criterios a imprimir";
+            this.groupBox1.Text = "Parámetros para el reporte:";
             // 
             // label1
             // 
@@ -70,7 +70,7 @@
             this.dateTimePicker1.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.Location = new System.Drawing.Point(19, 67);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(347, 28);
+            this.dateTimePicker1.Size = new System.Drawing.Size(335, 28);
             this.dateTimePicker1.TabIndex = 4;
             // 
             // button_generate
@@ -78,7 +78,7 @@
             this.button_generate.BackColor = System.Drawing.Color.SteelBlue;
             this.button_generate.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_generate.ForeColor = System.Drawing.Color.White;
-            this.button_generate.Location = new System.Drawing.Point(338, 515);
+            this.button_generate.Location = new System.Drawing.Point(151, 369);
             this.button_generate.Name = "button_generate";
             this.button_generate.Size = new System.Drawing.Size(138, 51);
             this.button_generate.TabIndex = 22;
@@ -96,19 +96,11 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "Fecha final:";
             // 
-            // dateTimePicker2
-            // 
-            this.dateTimePicker2.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker2.Location = new System.Drawing.Point(19, 150);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(347, 28);
-            this.dateTimePicker2.TabIndex = 6;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(455, 41);
+            this.label3.Location = new System.Drawing.Point(15, 213);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(95, 23);
             this.label3.TabIndex = 7;
@@ -117,17 +109,25 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(459, 68);
+            this.comboBox1.Location = new System.Drawing.Point(19, 240);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(170, 31);
+            this.comboBox1.Size = new System.Drawing.Size(335, 31);
             this.comboBox1.TabIndex = 8;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(19, 150);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(335, 28);
+            this.dateTimePicker2.TabIndex = 9;
             // 
             // GenerateSalesReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(811, 596);
+            this.ClientSize = new System.Drawing.Size(458, 443);
             this.Controls.Add(this.button_generate);
             this.Controls.Add(this.groupBox1);
             this.Name = "GenerateSalesReport";
