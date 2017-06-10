ab7ca7a092acbfafd8c66ac40237c75a17c1e0d9
flordemaria <fnoriegac@pucp.pe>
2017-06-10 17:50:32 -0500

    vista del reporte de ventas

--- InkaArt/InkaArt.Interface/Sales/SalesReport.Designer.cs
+++ InkaArt/InkaArt.Interface/Sales/SalesReport.Designer.cs
@@ -28,8 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_todaydate = new System.Windows.Forms.Label();
+            this.grid_salesReport = new System.Windows.Forms.DataGridView();
+            this.idDoc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.client = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.monto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_salesReport)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -37,7 +49,7 @@
             this.button1.BackColor = System.Drawing.Color.SteelBlue;
             this.button1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(378, 471);
+            this.button1.Location = new System.Drawing.Point(365, 466);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(154, 51);
             this.button1.TabIndex = 19;
@@ -48,11 +60,94 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(38, 36);
+            this.label1.Location = new System.Drawing.Point(35, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 23);
             this.label1.TabIndex = 20;
             this.label1.Text = "Producto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(476, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 23);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Fecha final:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 23);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Fecha inicial:";
+            // 
+            // label_todaydate
+            // 
+            this.label_todaydate.AutoSize = true;
+            this.label_todaydate.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_todaydate.Location = new System.Drawing.Point(748, 9);
+            this.label_todaydate.Name = "label_todaydate";
+            this.label_todaydate.Size = new System.Drawing.Size(0, 23);
+            this.label_todaydate.TabIndex = 23;
+            // 
+            // grid_salesReport
+            // 
+            this.grid_salesReport.AllowUserToAddRows = false;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grid_salesReport.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.grid_salesReport.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grid_salesReport.BackgroundColor = System.Drawing.SystemColors.InactiveBorder;
+            this.grid_salesReport.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid_salesReport.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.grid_salesReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_salesReport.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDoc,
+            this.date,
+            this.client,
+            this.amount,
+            this.monto});
+            this.grid_salesReport.Location = new System.Drawing.Point(39, 153);
+            this.grid_salesReport.Margin = new System.Windows.Forms.Padding(0, 4, 4, 4);
+            this.grid_salesReport.Name = "grid_salesReport";
+            this.grid_salesReport.Size = new System.Drawing.Size(820, 255);
+            this.grid_salesReport.TabIndex = 28;
+            // 
+            // idDoc
+            // 
+            this.idDoc.HeaderText = "N° Documento";
+            this.idDoc.Name = "idDoc";
+            // 
+            // date
+            // 
+            this.date.HeaderText = "Fecha";
+            this.date.Name = "date";
+            // 
+            // client
+            // 
+            this.client.HeaderText = "Cliente";
+            this.client.Name = "client";
+            // 
+            // amount
+            // 
+            this.amount.HeaderText = "Cantidad";
+            this.amount.Name = "amount";
+            // 
+            // monto
+            // 
+            this.monto.HeaderText = "Monto (S/.)";
+            this.monto.Name = "monto";
             // 
             // SalesReport
             // 
@@ -60,10 +155,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(899, 553);
+            this.Controls.Add(this.grid_salesReport);
+            this.Controls.Add(this.label_todaydate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "SalesReport";
             this.Text = "Reporte de Ventas";
+            ((System.ComponentModel.ISupportInitialize)(this.grid_salesReport)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,5 +172,14 @@
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label_todaydate;
+        private System.Windows.Forms.DataGridView grid_salesReport;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDoc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn client;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn monto;
     }
 }
