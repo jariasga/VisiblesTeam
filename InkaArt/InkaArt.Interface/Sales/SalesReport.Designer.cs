34a48f4f365f07d0676555b498727936e2ff4f5b
flordemaria <fnoriegac@pucp.pe>
2017-06-10 12:49:18 -0500

    forma del reporte de ventas

--- InkaArt/InkaArt.Interface/Sales/SalesReport.Designer.cs
+++ InkaArt/InkaArt.Interface/Sales/SalesReport.Designer.cs
@@ -28,58 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.listBox1.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 21;
-            this.listBox1.Items.AddRange(new object[] {
-            "Reporte de Ventas del 23 de marzo del 2017 al 23 de abril del 2017",
-            "",
-            "\t\tCantidad vendida     Ventas (en UM)     Compras (en UM)     Rentabilidad",
-            "",
-            "Producto 1\t           2500\t        10000\t        9000\t       11.11%",
-            "   ",
-            "Producto 2\t           4500\t         8000\t        6000\t       33.33%"});
-            this.listBox1.Location = new System.Drawing.Point(44, 51);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(814, 214);
-            this.listBox1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.SteelBlue;
             this.button1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(371, 310);
+            this.button1.Location = new System.Drawing.Point(378, 471);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(154, 51);
             this.button1.TabIndex = 19;
             this.button1.Text = "🖶 Imprimir";
             this.button1.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 23);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Producto:";
+            // 
             // SalesReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(899, 393);
+            this.ClientSize = new System.Drawing.Size(899, 553);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox1);
             this.Name = "SalesReport";
             this.Text = "Reporte de Ventas";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
