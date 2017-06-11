9e6e644cc2819081de680c5f8c1d07e1784b6efd
flordemaria <fnoriegac@pucp.pe>
2017-06-11 10:50:49 -0500

    view sales report

--- InkaArt/InkaArt.Interface/Sales/GenerateSalesReport.Designer.cs
+++ InkaArt/InkaArt.Interface/Sales/GenerateSalesReport.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dateTimePicker_fechaIni = new System.Windows.Forms.DateTimePicker();
             this.button_generateSalesResport = new System.Windows.Forms.Button();
+            this.comboBox_products = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.dateTimePicker_fechaFin = new System.Windows.Forms.DateTimePicker();
-            this.label3 = new System.Windows.Forms.Label();
-            this.comboBox_products = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker_fechaIni = new System.Windows.Forms.DateTimePicker();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,24 +56,6 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Parámetros para el reporte";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(24, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 23);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Fecha inicial:";
-            // 
-            // dateTimePicker_fechaIni
-            // 
-            this.dateTimePicker_fechaIni.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker_fechaIni.Location = new System.Drawing.Point(28, 77);
-            this.dateTimePicker_fechaIni.Name = "dateTimePicker_fechaIni";
-            this.dateTimePicker_fechaIni.Size = new System.Drawing.Size(340, 28);
-            this.dateTimePicker_fechaIni.TabIndex = 4;
-            // 
             // button_generateSalesResport
             // 
             this.button_generateSalesResport.BackColor = System.Drawing.Color.SteelBlue;
@@ -86,6 +68,24 @@
             this.button_generateSalesResport.Text = "Generar";
             this.button_generateSalesResport.UseVisualStyleBackColor = false;
             this.button_generateSalesResport.Click += new System.EventHandler(this.button_generateSalesResport_Click);
+            // 
+            // comboBox_products
+            // 
+            this.comboBox_products.FormattingEnabled = true;
+            this.comboBox_products.Location = new System.Drawing.Point(471, 77);
+            this.comboBox_products.Name = "comboBox_products";
+            this.comboBox_products.Size = new System.Drawing.Size(225, 31);
+            this.comboBox_products.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(467, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Producto:";
             // 
             // label2
             // 
@@ -105,23 +105,23 @@
             this.dateTimePicker_fechaFin.Size = new System.Drawing.Size(340, 28);
             this.dateTimePicker_fechaFin.TabIndex = 6;
             // 
-            // label3
+            // label1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(467, 51);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 23);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Producto:";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fecha inicial:";
             // 
-            // comboBox_products
+            // dateTimePicker_fechaIni
             // 
-            this.comboBox_products.FormattingEnabled = true;
-            this.comboBox_products.Location = new System.Drawing.Point(471, 74);
-            this.comboBox_products.Name = "comboBox_products";
-            this.comboBox_products.Size = new System.Drawing.Size(225, 31);
-            this.comboBox_products.TabIndex = 8;
+            this.dateTimePicker_fechaIni.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker_fechaIni.Location = new System.Drawing.Point(28, 77);
+            this.dateTimePicker_fechaIni.Name = "dateTimePicker_fechaIni";
+            this.dateTimePicker_fechaIni.Size = new System.Drawing.Size(340, 28);
+            this.dateTimePicker_fechaIni.TabIndex = 4;
             // 
             // GenerateSalesReport
             // 
