67c507dbd34786845cf5152b82a6eb2af6f9d87b
galan000 <a20102422@pucp.pe>
2017-06-20 17:12:32 -0500

    Nueva grabacion

--- InkaArt/InkaArt.Interface/Warehouse/WarehouseIndex.Designer.cs
+++ InkaArt/InkaArt.Interface/Warehouse/WarehouseIndex.Designer.cs
@@ -80,7 +80,7 @@
             this.textBox_description.Font = new System.Drawing.Font("Arial", 11F);
             this.textBox_description.Location = new System.Drawing.Point(616, 50);
             this.textBox_description.Name = "textBox_description";
-            this.textBox_description.Size = new System.Drawing.Size(334, 24);
+            this.textBox_description.Size = new System.Drawing.Size(334, 29);
             this.textBox_description.TabIndex = 30;
             // 
             // label2
@@ -88,7 +88,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(613, 27);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 18);
+            this.label2.Size = new System.Drawing.Size(112, 23);
             this.label2.TabIndex = 29;
             this.label2.Text = "Descripción";
             // 
@@ -98,7 +98,7 @@
             this.textBox_address.Font = new System.Drawing.Font("Arial", 11F);
             this.textBox_address.Location = new System.Drawing.Point(240, 110);
             this.textBox_address.Name = "textBox_address";
-            this.textBox_address.Size = new System.Drawing.Size(334, 24);
+            this.textBox_address.Size = new System.Drawing.Size(334, 29);
             this.textBox_address.TabIndex = 28;
             // 
             // comboBox_status
@@ -110,8 +110,9 @@
             "Inactivo"});
             this.comboBox_status.Location = new System.Drawing.Point(25, 110);
             this.comboBox_status.Name = "comboBox_status";
-            this.comboBox_status.Size = new System.Drawing.Size(192, 25);
+            this.comboBox_status.Size = new System.Drawing.Size(192, 29);
             this.comboBox_status.TabIndex = 27;
+            this.comboBox_status.Visible = false;
             // 
             // textBox_id
             // 
@@ -119,7 +120,7 @@
             this.textBox_id.Font = new System.Drawing.Font("Arial", 11F);
             this.textBox_id.Location = new System.Drawing.Point(25, 50);
             this.textBox_id.Name = "textBox_id";
-            this.textBox_id.Size = new System.Drawing.Size(192, 24);
+            this.textBox_id.Size = new System.Drawing.Size(192, 29);
             this.textBox_id.TabIndex = 26;
             // 
             // label5
@@ -127,7 +128,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(21, 27);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(23, 18);
+            this.label5.Size = new System.Drawing.Size(30, 23);
             this.label5.TabIndex = 25;
             this.label5.Text = "ID";
             // 
@@ -136,7 +137,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(237, 89);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 18);
+            this.label4.Size = new System.Drawing.Size(91, 23);
             this.label4.TabIndex = 24;
             this.label4.Text = "Dirección";
             // 
@@ -145,9 +146,10 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(21, 89);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 18);
+            this.label3.Size = new System.Drawing.Size(72, 23);
             this.label3.TabIndex = 20;
             this.label3.Text = "Estado";
+            this.label3.Visible = false;
             // 
             // textBox_name
             // 
@@ -155,7 +157,7 @@
             this.textBox_name.Font = new System.Drawing.Font("Arial", 11F);
             this.textBox_name.Location = new System.Drawing.Point(240, 50);
             this.textBox_name.Name = "textBox_name";
-            this.textBox_name.Size = new System.Drawing.Size(334, 24);
+            this.textBox_name.Size = new System.Drawing.Size(334, 29);
             this.textBox_name.TabIndex = 17;
             // 
             // label1
@@ -163,7 +165,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(237, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 18);
+            this.label1.Size = new System.Drawing.Size(79, 23);
             this.label1.TabIndex = 16;
             this.label1.Text = "Nombre";
             // 
@@ -270,7 +272,7 @@
             // 
             // WarehouseIndex
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1025, 518);
