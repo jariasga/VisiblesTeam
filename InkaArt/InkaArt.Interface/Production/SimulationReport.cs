c33aae04ffcb53b77f2c0fa09f8ddde6b0b36a09
Reechee <ricardo751991@gmail.com>
2017-06-27 16:32:12 -0500

    simulation report

--- InkaArt/InkaArt.Interface/Production/SimulationReport.cs
+++ InkaArt/InkaArt.Interface/Production/SimulationReport.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 using InkaArt.Business.Reports;
 using InkaArt.Business.Algorithm;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace InkaArt.Interface.Production
 {
@@ -75,9 +78,65 @@ namespace InkaArt.Interface.Production
             xlexcel.Visible = true;
             xlWorkBook = xlexcel.Workbooks.Add(misValue);
             xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-            Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[1, 1];
+            for (int j = 0; j < simulation_grid.Columns.Count; ++j)
+                xlWorkSheet.Cells[1, j + 1] = simulation_grid.Columns[j].HeaderText;
+            Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[2, 1];
             CR.Select();
             xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+        }
+
+        private void button_pdf_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("ReporteStocks.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+            Document document = new Document(PageSize.A4);
+            PdfWriter writer = PdfWriter.GetInstance(document, fs);
+            document.Open();
+            var boldFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10);
+            var titleFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 20);
+
+            Paragraph title = new Paragraph("REPORTE DE STOCKS", titleFont);
+            title.Alignment = Element.ALIGN_CENTER;
+            document.Add(title);
+            document.Add(new Paragraph(" "));
+
+            var phrase = new Phrase();
+            phrase.Add(new Chunk("Fecha de generación de reporte:    ", boldFont));
+            phrase.Add(new Chunk(label_todaydate.Text));
+            document.Add(new Paragraph(phrase));
+            document.Add(new Paragraph(" "));
+
+            PdfPTable table = new PdfPTable(6)
+            {
+                WidthPercentage = 100,
+            };
+
+            for (int i = 0; i < simulation_grid.Columns.Count; i++)
+            {
+                table.AddCell(simulation_grid.Columns[i].HeaderText);
+            }
+            for (int i = 0; i < simulation_grid.Rows.Count; i++)
+            {
+                for (int j = 0; j < simulation_grid.Columns.Count; j++)
+                {
+                    if (simulation_grid.Rows[i].Cells[j].Value != null)
+                    {
+                        if (j == 0)
+                        {
+                            table.AddCell(simulation_grid.Rows[i].Cells[j].Value.ToString().Substring(0, 10));
+                        } else
+                        table.AddCell(simulation_grid.Rows[i].Cells[j].Value.ToString());
+                    }
+                    else
+                    {
+                        table.AddCell("");
+                    }
+                }
+            }
+            document.Add(table);
+            document.Add(new Paragraph(" "));
+
+            document.Close();
+            MessageBox.Show("Se generó el archivo del reporte de desempeño de trabajadores exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
--- InkaArt/InkaArt.Interface/Warehouse/StockReport.cs
+++ InkaArt/InkaArt.Interface/Warehouse/StockReport.cs
@@ -62,7 +62,9 @@ namespace InkaArt.Interface.Warehouse
             xlexcel.Visible = true;
             xlWorkBook = xlexcel.Workbooks.Add(misValue);
             xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-            Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[1, 1];
+            for (int j = 0; j < dataGrid_stocks.Columns.Count; ++j)
+                xlWorkSheet.Cells[1, j + 1] = dataGrid_stocks.Columns[j].HeaderText;
+            Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[2, 1];
             CR.Select();
             xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
         }
