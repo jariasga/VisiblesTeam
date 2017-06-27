5faf3ed00f3c6f6e9dbc6a707c7463602fbe75c8
flordemaria <fnoriegac@pucp.pe>
2017-06-27 15:28:14 -0500

    el resto de reportes listos

--- InkaArt/InkaArt.Interface/Warehouse/KardexReport.Designer.cs
+++ InkaArt/InkaArt.Interface/Warehouse/KardexReport.Designer.cs
@@ -28,14 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle16 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle17 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle18 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button_export = new System.Windows.Forms.Button();
             this.dataGridView_movements = new System.Windows.Forms.DataGridView();
-            this.label_todaydate = new System.Windows.Forms.Label();
             this.Producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.idMov = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TipoMov = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -43,6 +42,8 @@
             this.nombAlm = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Item = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label_todaydate = new System.Windows.Forms.Label();
+            this.button_pdf = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_movements)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,28 +52,29 @@
             this.button_export.BackColor = System.Drawing.Color.SteelBlue;
             this.button_export.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_export.ForeColor = System.Drawing.Color.White;
-            this.button_export.Location = new System.Drawing.Point(629, 495);
+            this.button_export.Location = new System.Drawing.Point(466, 535);
             this.button_export.Name = "button_export";
-            this.button_export.Size = new System.Drawing.Size(154, 51);
+            this.button_export.Size = new System.Drawing.Size(249, 51);
             this.button_export.TabIndex = 18;
-            this.button_export.Text = "Exportar";
+            this.button_export.Text = "Exportar a Excel";
             this.button_export.UseVisualStyleBackColor = false;
             this.button_export.Click += new System.EventHandler(this.button_export_Click);
             // 
             // dataGridView_movements
             // 
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dataGridView_movements.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle16.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView_movements.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle16;
+            this.dataGridView_movements.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView_movements.BackgroundColor = System.Drawing.SystemColors.InactiveBorder;
             this.dataGridView_movements.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView_movements.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle17.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle17.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle17.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle17.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle17.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle17.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle17.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView_movements.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle17;
             this.dataGridView_movements.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView_movements.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Producto,
@@ -82,39 +84,30 @@
             this.nombAlm,
             this.Item,
             this.cant});
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView_movements.DefaultCellStyle = dataGridViewCellStyle3;
-            this.dataGridView_movements.Location = new System.Drawing.Point(58, 94);
+            dataGridViewCellStyle18.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle18.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle18.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle18.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle18.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle18.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle18.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView_movements.DefaultCellStyle = dataGridViewCellStyle18;
+            this.dataGridView_movements.Location = new System.Drawing.Point(50, 96);
             this.dataGridView_movements.Margin = new System.Windows.Forms.Padding(0, 4, 4, 4);
             this.dataGridView_movements.Name = "dataGridView_movements";
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView_movements.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dataGridView_movements.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle19.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle19.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle19.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle19.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle19.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle19.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle19.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView_movements.RowHeadersDefaultCellStyle = dataGridViewCellStyle19;
+            dataGridViewCellStyle20.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView_movements.RowsDefaultCellStyle = dataGridViewCellStyle20;
             this.dataGridView_movements.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dataGridView_movements.Size = new System.Drawing.Size(1278, 372);
+            this.dataGridView_movements.Size = new System.Drawing.Size(1315, 372);
             this.dataGridView_movements.TabIndex = 25;
-            // 
-            // label_todaydate
-            // 
-            this.label_todaydate.AutoSize = true;
-            this.label_todaydate.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_todaydate.Location = new System.Drawing.Point(1253, 9);
-            this.label_todaydate.Name = "label_todaydate";
-            this.label_todaydate.Size = new System.Drawing.Size(0, 23);
-            this.label_todaydate.TabIndex = 26;
             // 
             // Producto
             // 
@@ -131,49 +124,67 @@
             this.idMov.FillWeight = 21.912F;
             this.idMov.HeaderText = "ID Movimiento";
             this.idMov.Name = "idMov";
-            this.idMov.Width = 170;
             // 
             // TipoMov
             // 
             this.TipoMov.FillWeight = 21.912F;
             this.TipoMov.HeaderText = "Tipo de Movimiento";
             this.TipoMov.Name = "TipoMov";
-            this.TipoMov.Width = 171;
             // 
             // Reason
             // 
             this.Reason.FillWeight = 21.912F;
             this.Reason.HeaderText = "Razón";
             this.Reason.Name = "Reason";
-            this.Reason.Width = 170;
             // 
             // nombAlm
             // 
             this.nombAlm.FillWeight = 21.912F;
             this.nombAlm.HeaderText = "Almacén";
             this.nombAlm.Name = "nombAlm";
-            this.nombAlm.Width = 170;
             // 
             // Item
             // 
             this.Item.FillWeight = 21.912F;
             this.Item.HeaderText = "Item";
             this.Item.Name = "Item";
-            this.Item.Width = 171;
             // 
             // cant
             // 
             this.cant.FillWeight = 21.912F;
             this.cant.HeaderText = "Cantidad";
             this.cant.Name = "cant";
-            this.cant.Width = 170;
+            // 
+            // label_todaydate
+            // 
+            this.label_todaydate.AutoSize = true;
+            this.label_todaydate.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_todaydate.Location = new System.Drawing.Point(1299, 9);
+            this.label_todaydate.Name = "label_todaydate";
+            this.label_todaydate.Size = new System.Drawing.Size(0, 23);
+            this.label_todaydate.TabIndex = 26;
+            // 
+            // button_pdf
+            // 
+            this.button_pdf.BackColor = System.Drawing.Color.SteelBlue;
+            this.button_pdf.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_pdf.ForeColor = System.Drawing.Color.White;
+            this.button_pdf.Location = new System.Drawing.Point(741, 535);
+            this.button_pdf.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_pdf.Name = "button_pdf";
+            this.button_pdf.Size = new System.Drawing.Size(249, 50);
+            this.button_pdf.TabIndex = 40;
+            this.button_pdf.Text = "Exportar a PDF";
+            this.button_pdf.UseVisualStyleBackColor = false;
+            this.button_pdf.Click += new System.EventHandler(this.button_pdf_Click);
             // 
             // KardexReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1430, 578);
+            this.ClientSize = new System.Drawing.Size(1430, 615);
+            this.Controls.Add(this.button_pdf);
             this.Controls.Add(this.label_todaydate);
             this.Controls.Add(this.dataGridView_movements);
             this.Controls.Add(this.button_export);
@@ -198,5 +209,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn nombAlm;
         private System.Windows.Forms.DataGridViewTextBoxColumn Item;
         private System.Windows.Forms.DataGridViewTextBoxColumn cant;
+        private System.Windows.Forms.Button button_pdf;
     }
 }
--- InkaArt/InkaArt.Interface/Warehouse/KardexReport.cs
+++ InkaArt/InkaArt.Interface/Warehouse/KardexReport.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using InkaArt.Business.Reports;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace InkaArt.Interface.Warehouse
 {
@@ -64,6 +67,61 @@ namespace InkaArt.Interface.Warehouse
             Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[1, 1];
             CR.Select();
             xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+        }
+
+        private void button_pdf_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("ReporteKardex.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+            Document document = new Document(PageSize.A4);
+            PdfWriter writer = PdfWriter.GetInstance(document, fs);
+            document.Open();
+            var boldFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10);
+            var titleFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 20);
+
+            Paragraph title = new Paragraph("REPORTE KARDEX", titleFont);
+            title.Alignment = Element.ALIGN_CENTER;
+            document.Add(title);
+            document.Add(new Paragraph(" "));
+
+            var phrase = new Phrase();
+            phrase.Add(new Chunk("Fecha de generación de reporte:    ", boldFont));
+            phrase.Add(new Chunk(label_todaydate.Text));
+            document.Add(new Paragraph(phrase));
+            document.Add(new Paragraph(" "));
+
+            PdfPTable table = new PdfPTable(7)
+            {
+                WidthPercentage = 100,
+            };
+
+            for (int i = 0; i < dataGridView_movements.Columns.Count; i++)
+            {
+                table.AddCell(dataGridView_movements.Columns[i].HeaderText);
+            }
+            for (int i = 0; i < dataGridView_movements.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView_movements.Columns.Count; j++)
+                {
+                    if (dataGridView_movements.Rows[i].Cells[j].Value != null)
+                    {
+                        if (j == 0)
+                        {
+                            table.AddCell(dataGridView_movements.Rows[i].Cells[j].Value.ToString().Substring(0, 10));
+                        }
+                        else
+                            table.AddCell(dataGridView_movements.Rows[i].Cells[j].Value.ToString());
+                    }
+                    else
+                    {
+                        table.AddCell("");
+                    }
+                }
+            }
+            document.Add(table);
+            document.Add(new Paragraph(" "));
+
+            document.Close();
+            MessageBox.Show("Se generó el archivo del reporte de desempeño de trabajadores exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
