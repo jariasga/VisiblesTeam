3f03c2503588609545f13eb36394d7575cecc6c3
nataliapalomares <npalomares@pucp.pe>
2017-06-26 22:37:40 -0500

    Ya esta la carga masiva de lista de materias provistas

--- InkaArt/InkaArt.Interface/Purchases/Suppliers.cs
+++ InkaArt/InkaArt.Interface/Purchases/Suppliers.cs
@@ -174,8 +174,24 @@ namespace InkaArt.Interface.Purchases
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Title = "Open Suppliers File";
             dialog.Filter = "CSV files|*.csv";
-            if (dialog.ShowDialog() == DialogResult.OK) { 
-                if(control.massiveUpload(dialog.FileName)==1) MessageBox.Show("No se pudo cargar el archivo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                if (control.massiveUpload(dialog.FileName) == 1)
+                {
+                    MessageBox.Show("No se pudo cargar el archivo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            OpenFileDialog dialog2 = new OpenFileDialog();
+            dialog2.Title = "Open Raw materials offered by suppliers File";
+            dialog2.Filter = "CSV files|*.csv";
+            if (dialog2.ShowDialog() == DialogResult.OK)
+            {
+                RawMaterial_SupplierController control_rm=new RawMaterial_SupplierController();
+                if (control_rm.massiveUpload(dialog2.FileName) == 1)
+                {
+                    MessageBox.Show("No se pudo cargar el archivo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
             }
             textBox_id.Text = "";
             textBox_ruc.Text = "";
