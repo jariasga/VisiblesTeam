d47635473b039d8dd0706d20ec2db4cb3b4939be
camilamilagros <camila.chavez@pucp.pe>
2017-07-07 19:23:59 -0500

    forms

--- InkaArt/InkaArt.Interface/Warehouse/SaleMovement.Designer.cs
+++ InkaArt/InkaArt.Interface/Warehouse/SaleMovement.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonDelete = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button3 = new System.Windows.Forms.Button();
@@ -52,30 +51,16 @@
             this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // buttonDelete
-            // 
-            this.buttonDelete.BackColor = System.Drawing.Color.Firebrick;
-            this.buttonDelete.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonDelete.ForeColor = System.Drawing.Color.White;
-            this.buttonDelete.Location = new System.Drawing.Point(322, 460);
-            this.buttonDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(138, 39);
-            this.buttonDelete.TabIndex = 57;
-            this.buttonDelete.Text = "Cancelar";
-            this.buttonDelete.UseVisualStyleBackColor = false;
-            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
-            // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.SteelBlue;
             this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(131, 460);
+            this.button2.Location = new System.Drawing.Point(350, 471);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(138, 39);
             this.button2.TabIndex = 56;
-            this.button2.Text = "Aceptar";
+            this.button2.Text = "🖫 Guardar";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -86,22 +71,22 @@
             this.groupBox2.Controls.Add(this.text_order);
             this.groupBox2.Controls.Add(this.dataGridView1);
             this.groupBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(28, 173);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Location = new System.Drawing.Point(28, 148);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox2.Size = new System.Drawing.Size(577, 265);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(784, 307);
             this.groupBox2.TabIndex = 58;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Número de pedido";
+            this.groupBox2.Text = "Pedido";
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Gray;
             this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(524, 20);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(719, 32);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(45, 31);
             this.button3.TabIndex = 57;
@@ -112,17 +97,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(283, 26);
+            this.label1.Location = new System.Drawing.Point(431, 38);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 18);
+            this.label1.Size = new System.Drawing.Size(138, 18);
             this.label1.TabIndex = 34;
-            this.label1.Text = "Nro Pedido:";
+            this.label1.Text = "Número de pedido";
             // 
             // text_order
             // 
-            this.text_order.Location = new System.Drawing.Point(377, 24);
-            this.text_order.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.text_order.Location = new System.Drawing.Point(573, 35);
+            this.text_order.Margin = new System.Windows.Forms.Padding(2);
             this.text_order.Name = "text_order";
             this.text_order.Size = new System.Drawing.Size(142, 26);
             this.text_order.TabIndex = 34;
@@ -140,10 +125,10 @@
             this.MovementCant,
             this.MovementQuantity,
             this.Modificar});
-            this.dataGridView1.Location = new System.Drawing.Point(4, 63);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(19, 83);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(565, 187);
+            this.dataGridView1.Size = new System.Drawing.Size(745, 199);
             this.dataGridView1.TabIndex = 57;
             // 
             // idProduct
@@ -193,47 +178,47 @@
             this.groupBox3.Controls.Add(this.textBox6);
             this.groupBox3.Controls.Add(this.label9);
             this.groupBox3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(28, 40);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Location = new System.Drawing.Point(28, 23);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox3.Size = new System.Drawing.Size(577, 104);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(784, 121);
             this.groupBox3.TabIndex = 58;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Almacén";
             // 
             // text_id_warehouse
             // 
-            this.text_id_warehouse.Location = new System.Drawing.Point(411, 57);
-            this.text_id_warehouse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.text_id_warehouse.Location = new System.Drawing.Point(19, 62);
+            this.text_id_warehouse.Margin = new System.Windows.Forms.Padding(2);
             this.text_id_warehouse.Name = "text_id_warehouse";
             this.text_id_warehouse.ReadOnly = true;
-            this.text_id_warehouse.Size = new System.Drawing.Size(142, 26);
+            this.text_id_warehouse.Size = new System.Drawing.Size(202, 26);
             this.text_id_warehouse.TabIndex = 33;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(418, 36);
+            this.label5.Location = new System.Drawing.Point(16, 42);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(20, 18);
+            this.label5.Size = new System.Drawing.Size(23, 18);
             this.label5.TabIndex = 32;
-            this.label5.Text = "Id";
+            this.label5.Text = "ID";
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(8, 57);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox6.Location = new System.Drawing.Point(240, 62);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
             this.textBox6.Name = "textBox6";
             this.textBox6.ReadOnly = true;
-            this.textBox6.Size = new System.Drawing.Size(362, 26);
+            this.textBox6.Size = new System.Drawing.Size(524, 26);
             this.textBox6.TabIndex = 29;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(4, 36);
+            this.label9.Location = new System.Drawing.Point(236, 41);
             this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(64, 18);
@@ -245,13 +230,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(626, 522);
+            this.ClientSize = new System.Drawing.Size(839, 522);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.button2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "SaleMovement";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SaleMovementcs";
@@ -265,7 +249,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button buttonDelete;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button3;
