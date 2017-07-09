b10f1191fb2e19cbf178192faa5404e53bfef78d
Mauricio <Mauricio>
2017-07-08 19:34:16 -0500

    correct it

--- InkaArt/InkaArt.Data/Sales/OrderData.cs
+++ InkaArt/InkaArt.Data/Sales/OrderData.cs
@@ -564,7 +564,6 @@ namespace InkaArt.Data.Sales
                 if (isClientSelected)
                 {
                     updateLogicalStock(currentProductId, cant, type);
-                    lineStatus = "despachado";
                     produced = cant;
                 }
                 row["quantityProduced"] = produced;
